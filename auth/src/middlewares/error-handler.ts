75890c4e4aa7895676538beed14f9c81d3b26ea8
Önder ALKAN <onder.alkan15@gmail.com>
2022-08-08 22:46:49 +0300

    test commit1

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,4 @@
 import type { NextFunction, Request, Response } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { CustomError } from "../errors/custom-error";
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
